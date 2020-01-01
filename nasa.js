dffb501bda9121c0ecb50c4ee3ed9daf6674da65
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-01 14:17:01 -0500

    adding date algorithm, in progress

--- nasa.js
+++ nasa.js
@@ -1,29 +1,27 @@
-document.getElementById('search').addEventListener('click', onload_function);
+document.getElementById('search').addEventListener('click', search_function);
+
+document.getElementById('prevday').addEventListener('click', previousday_function);
+
+document.getElementById('nxtday').addEventListener('click', nextday_function);
 
     let width = this.innerWidth, //this refers to window
         height = this.innerHeight,
-    //this will be for getting the current date
-    computerDate = new Date(),
-    dd = String(computerDate.getDate()).padStart(2, '0'),
-    mm = String(computerDate.getMonth() + 1).padStart(2, '0'), //January is 0!
-    yyyy = computerDate.getFullYear(),
-    today = yyyy + '-' + mm + '-' + dd;
+    //this will be for getting the current date onload
+        computerDate = new Date(),
+        dd = String(computerDate.getDate()).padStart(2, '0'),
+        mm = String(computerDate.getMonth() + 1).padStart(2, '0'), //January is 0!
+        yyyy = computerDate.getFullYear(),
+        today = yyyy + '-' + mm + '-' + dd;
 
-onload_function()
-function onload_function() {
+    console.log(today.substring(8,10));
+
+call_api_function()
+function call_api_function() {
 
     let xhr = new XMLHttpRequest() , endpoint = `https://api.nasa.gov/planetary/apod`, myKey = `?api_key=Fgrr1lJp3BQ5AUgM9k0EuSkLS0R9RhUKbSavz4dP`;
 
-    if (document.getElementById('dateinput').value == '') {
-
-        endpoint += myKey + "&date=" + today + '&hd=true';
-        
-    } else {
-
-        endpoint += myKey + '&date=' + document.getElementById('dateinput').value + '&hd=true';
-
-    }
-
+    endpoint += myKey + "&date=" + today + '&hd=true';
+    
     console.log(endpoint);
 
     xhr.open('GET', endpoint ,true);
@@ -37,7 +35,7 @@ function onload_function() {
         if (nasaData.code == '400') {
             document.getElementById('dateinput').value = '';
             document.getElementById('dateinput').placeholder = 'Please enter date in the correct format.';
-        } else {
+        }  else {
 
             document.getElementById('image').src = nasaData.hdurl;
 
@@ -52,5 +50,31 @@ function onload_function() {
     }
 
     xhr.send();
+}
+
+function search_function() {
+    today = document.getElementById('dateinput').value;
+    call_api_function()
+}
+
+function previousday_function() {
+
+   if (today.substring(8,10) == "01" ) {
+       today = yyyy + '-' + mm + '-' + dd;
+   } else {
+       today = yyyy + '-' + mm + '-' + (parseInt(dd) - 1);
+   }
+
+    call_api_function()
+}
+
+function nextday_function() {
+    if (document.getElementById('dateinput').value == "") {
+        today = yyyy + '-' + mm + '-' + (parseInt(dd) + 1);
+    } else {
+        today = document.getElementById('dateinput').value;
+    }
+
+    call_api_function()
 }
 
